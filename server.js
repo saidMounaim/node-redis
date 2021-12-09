739d2dbffb943171a01af22bfed3db6c514caa52
Said Mounaim <saidmounaim00@gmail.com>
2021-12-09 22:54:21 +0100

    Connect Database With Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(201).json({ success: true, message: "Welcome To Node Redis API" });
