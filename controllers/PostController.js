a23b7b0accf271fb30a4d3fb0ae9866268f9dfe8
Said Mounaim <saidmounaim00@gmail.com>
2021-12-10 00:00:37 +0100

    Add Redis

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,8 @@
 import asyncHandler from "express-async-handler";
 import Post from "../models/Post.js";
+import asyncRedis from "async-redis";
+
+const client = asyncRedis.createClient();
 
 // @DESC Get All Post
 // @ROUTE /api/posts
@@ -7,7 +10,15 @@ import Post from "../models/Post.js";
 export const getAll = asyncHandler(async (req, res) => {
   try {
     const posts = await Post.find({});
-    res.status(201).json({ success: true, data: posts });
+
+    const postsCached = await client.get("postsCached");
+
+    if (postsCached) {
+      res.status(201).json({ success: true, data: postsCached });
+    } else {
+      res.status(201).json({ success: true, data: posts });
+      await client.set("postsCached", posts, "EX", 10);
+    }
   } catch (error) {
     console.log(error.message);
   }
@@ -19,7 +30,14 @@ export const getAll = asyncHandler(async (req, res) => {
 export const getSingle = asyncHandler(async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    res.status(201).json({ success: true, data: post });
+    const postCached = await client.get("postCached");
+
+    if (postCached) {
+      res.status(201).json({ success: true, data: postCached });
+    } else {
+      res.status(201).json({ success: true, data: post });
+      await client.set("postCached", post, "EX", 10);
+    }
   } catch (error) {
     console.log(error.message);
   }
